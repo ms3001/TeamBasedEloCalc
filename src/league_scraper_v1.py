2b7c45bfdc3393f4eabf02e0d9b8b06bcf5ee822
Pratyush Trivedi <pratyush.trivedi72@gmail.com>
2020-12-25 21:53:34 -0500

    added 5 matches pratyush was in (dbPickle.txt)

--- src/league_scraper_v1.py
+++ src/league_scraper_v1.py
@@ -26,7 +26,7 @@ def scrapeMatchHistoryUrl(usernametext, passwordtext, matchUrl):
 	submit = browser.find_element_by_css_selector(".mobile-button__submit")
 	submit.click()
 
-	time.sleep(10) #sleep while page loads
+	time.sleep(8) #sleep while page loads
 
 	page_source = browser.page_source
 	soup = BeautifulSoup(page_source, 'html.parser')
